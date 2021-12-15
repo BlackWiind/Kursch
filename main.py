d7978af3726f8db3b6880f7775f25e0aef8d885a
BlackWiind <BlackWiind@gmail.com>
2021-12-15 15:28:42 +1000

    рефакторинг

--- main.py
+++ main.py
@@ -14,77 +14,77 @@ xIp = []
 n = 1
 
 
-def horde(_a, _b):
-    return _a - function(_a) * (_b - _a) / (function(_b) - function(_a))
+def horde(local_a, local_b) -> float:
+    return local_a - math_function(local_a) * (local_b - local_a) / (math_function(local_b) - math_function(local_a))
 
 
-def function(_x):
-    return 3 * (_x ** 4) - 8 * (_x ** 3) - 18 * (_x ** 2) + 2
+def math_function(local_x) -> float:
+    return 3 * (local_x ** 4) - 8 * (local_x ** 3) - 18 * (local_x ** 2) + 2
 
 
-def function1(_x):
-    return 12 * (_x ** 3) - 24 * (_x ** 2) - 36 * _x
+def math_function1(local_x) -> float:
+    return 12 * (local_x ** 3) - 24 * (local_x ** 2) - 36 * local_x
 
 
-def function_rc(_x, _y):
-    return (6 - (_y ** 2)) * cos(_x) + 2 * _y
+def function_rc(local_x, local_y) -> float:
+    return (6 - (local_y ** 2)) * cos(local_x) + 2 * local_y
 
 
-def runge_Cutt_4(_x, _y, _h):
-    k1 = function_rc(_x, _y)
-    k2 = function_rc(_x + _h / 2, _y + _h / 2 * k1)
-    k3 = function_rc(_x + _h / 2, _y + _h / 2 * k2)
-    k4 = function_rc(_x + h, _y + h * k3)
-    return _y + _h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+def runge_Cutt_4(local_x, local_y, local_h) -> float:
+    k1 = function_rc(local_x, local_y)
+    k2 = function_rc(local_x + local_h / 2, local_y + local_h / 2 * k1)
+    k3 = function_rc(local_x + local_h / 2, local_y + local_h / 2 * k2)
+    k4 = function_rc(local_x + h, local_y + h * k3)
+    return local_y + local_h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
 
-def rC_calc(_h):
+def rC_calc(local_h) -> float:
     global n
     x2 = 0
     x1.append(0)
     y1.append(answer)
     y2.append(answer)
     while x1[-1] < 1:
-        y1.append(runge_Cutt_4(x1[-1], y1[-1], _h))
-        x1.append(x1[-1] + _h)
+        y1.append(runge_Cutt_4(x1[-1], y1[-1], local_h))
+        x1.append(x1[-1] + local_h)
     while x2 <= 1:
-        y2.append(runge_Cutt_4(x2, y2[-1], _h / 2))
-        x2 += (_h / 2)
+        y2.append(runge_Cutt_4(x2, y2[-1], local_h / 2))
+        x2 += (local_h / 2)
     for values in range(1, len(y1)):
         if abs(y1[values] - y2[values * 2]) < 0.0001 * 15:
             for index in range(len(y1)):
                 print("x = ", x1[index], "\ty = ", round(y1[index], 4))
             return
-    _h /= 2
+    local_h /= 2
     n *= 2
     y1.clear()
     y2.clear()
     x1.clear()
-    rC_calc(_h)
+    rC_calc(local_h)
 
 
-def interpolation(x: array, y: array, xIp: array, n: int, nIp: int) -> array:
+def interpolation(local_x: array, local_y: array, local_xIp: array, local_n: int, local_nIp: int) -> array:
     arr = []
-    h = x[1] - x[0]
-    for i in range(nIp):
+    h = local_x[1] - local_x[0]
+    for i in range(local_nIp):
         j = 0
-        while j < n and x[j] <= xIp[i]:
-            #print(i,'\t',j,'\t',n,'\t',x[j],'\t',xIp[i])
+        while j <= local_n or local_x[j] <= local_xIp[i]:
+            # print(i,'\t',j,'\t',n,'\t',x[j],'\t',xIp[i])
             j += 1
         j -= 1
-        q = (xIp[i] - x[j]) / h
-        arr.append(y[j] + q * (y[j + 1] - y[j]))
+        q = (local_xIp[i] - local_x[j]) / h
+        arr.append(local_y[j] + q * (local_y[j + 1] - local_y[j]))
     return arr
 
 
 while True:
     iterations += 1
     answer = horde(a, b)
-    if function(a) * function1(a) > 0:
+    if math_function(a) * math_function1(a) > 0:
         b = answer
     else:
         a = answer
-    if function(answer) <= accurcity:
+    if math_function(answer) <= accurcity:
         break
 rC_calc(h)
 nIp = math.trunc((1 - 0) / 0.1)
